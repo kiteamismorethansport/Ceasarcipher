7401de6afcdd50be57a5ffb74ca91f993a1dd7dd
Dmytro Kovalov <dmytroidmytroidmytro@gmail.com>
2022-05-18 17:56:39 +0300

    Changed the size of JFrame, buttons, textfields

--- src/main/СeaserEncryptFrame.java
+++ src/main/СeaserEncryptFrame.java
@@ -30,39 +30,57 @@ public class СeaserEncryptFrame extends JFrame {
         String texttoEcnrypt;
     СeaserEncryptFrame.eHandler handler = new СeaserEncryptFrame.eHandler();
 
-        public СeaserEncryptFrame () {
-            this.setLayout(new FlowLayout());
-            this.b1 = new JButton("Выбрать");
-            this.b2 = new JButton("Выбрать");
-            this.b3 = new JButton("Зашифровать");
-            this.b4 = new JButton("Расшифровать");
-            this.b5 = new JButton("BRUTEFORCE");
-            this.l1 = new JLabel("Введите адрес файла");
-            this.l2 = new JLabel("Введите ключ шифра");
-            this.l3 = new JLabel("");
-            this.l4 = new JLabel("");
-            this.l5 = new JLabel("");
-            this.l6 = new JLabel ("");
-            this.t1 = new JTextField(30);
-            this.t2 = new JTextField(10);
-            this.add(this.b1);
-            this.add(this.l1);
-            this.add(this.t1);
-            this.add(this.l3);
-            this.add(this.b2);
-            this.add(this.l2);
-            this.add(this.t2);
-            this.add(this.l4);
-            this.add(this.b3);
-            this.add(this.l5);
-            this.add(this.b4);
-            this.add(this.b5);
-            this.add(this.l6);
-            this.b1.addActionListener(this.handler);
-            this.b2.addActionListener(this.handler);
-            this.b3.addActionListener(this.handler);
-            this.b4.addActionListener(this.handler);
-            this.b5.addActionListener(this.handler);
+        public СeaserEncryptFrame (String s) {
+            super(s);
+            setLayout(new BorderLayout());
+            JPanel panel = new JPanel(null);
+            add(panel,BorderLayout.CENTER);
+
+            b1 = new JButton("Выбрать");
+            b1.setBounds(10,10,150,30);
+            b2 = new JButton("Выбрать");
+            b2.setBounds(10,50,150,30);
+            b3 = new JButton("Зашифровать");
+            b3.setBounds(10,90,150,30);
+            b4 = new JButton ("Расшифровать");
+            b4.setBounds(205,90,150,30);
+            b5 = new JButton("BRUTEFORCE");
+            b5.setBounds(405, 90, 150, 30);
+            l1 = new JLabel("Введите адрес файла");
+            l1.setBounds(165, 10, 200,30);
+            l2 = new JLabel("Введите ключ шифра");
+            l2.setBounds(165, 50, 150, 30);
+            l3 = new JLabel("");
+            l3.setBounds(10,160, 500,15);
+            l4 = new JLabel("");
+            l4.setBounds(10,180, 250,15);
+            l5 = new JLabel("");
+            l5.setBounds(10,200, 250,15);
+            l6 = new JLabel("");
+            l6.setBounds(10,220, 500,15);
+            t1 = new JTextField();
+            t1.setBounds(305, 10, 250, 30);
+            t2 = new JTextField();
+            t2.setBounds(305,50,250,30);
+            panel.add(b1);
+            panel.add(b2);
+            panel.add(b3);
+            panel.add(b4);
+            panel.add(b5);
+            panel.add(l1);
+            panel.add(l2);
+            panel.add(l3);
+            panel.add(l4);
+            panel.add(l5);
+            panel.add(l6);
+            panel.add(t1);
+            panel.add(t2);
+
+            b1.addActionListener(this.handler);
+            b2.addActionListener(this.handler);
+            b3.addActionListener(this.handler);
+            b4.addActionListener(this.handler);
+            b5.addActionListener(this.handler);
         }
 
     public class eHandler implements ActionListener {
@@ -113,7 +131,7 @@ public class СeaserEncryptFrame extends JFrame {
     }
 
     public static void main(String[] args) throws IOException {
-        СeaserEncryptFrame app = new СeaserEncryptFrame();
+        СeaserEncryptFrame app = new СeaserEncryptFrame("CeaserCipher");
         app.setVisible(true);
         app.setDefaultCloseOperation(3);
         app.setSize(600, 300);
